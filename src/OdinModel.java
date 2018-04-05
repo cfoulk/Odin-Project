bba48ac83d9191b8c11f12431f74df220a7e6548
ramyasingam <ramyasrisingams@csus.edu>
2018-04-05 15:13:21 -0700

    Task class in OdinModel

--- src/OdinModel.java
+++ src/OdinModel.java
@@ -114,7 +114,31 @@ class Project
 
 class Task
 {
-	
+	int dueDate;
+	int employeeID;
+	int projectID;
+	int size;
+	int taskID;
+	String name;
+	String description;
+
+	Task (ResultSet myRS) throws Exception
+	{
+		this.dueDate = myRS.getInt("DueDate");
+		this.employeeID = myRS.getInt("EmployeeID");
+		this.projectID = myRS.getInt("ProjectID");
+		this.name = myRS.getString("Name");
+		this.description = myRS.getString("Description");
+		this.size = myRS.getInt("Size");
+		this.taskID = myRS.getInt("TaskID");
+	}
+
+	String projectLine()
+	{
+		return (this.employeeID + "\t" + this.name + "\t" + this.projectID + "\t" +
+				this.description + "\t" + this.dueDate + "\t" + this.size + "\t" +
+				this.taskID);
+	}
 }
 
 class WorkLog
