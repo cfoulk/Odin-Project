eb67cbbb7ca899a22f5e27be467d6be34f5b8d4e
EvWoN <kuriosevwon@gmail.com>
2018-04-10 16:02:12 -0700

    Added Offsite

---
+++ src/Model/Offsite.java
@@ -0,0 +1,13 @@
+package Model;
+
+import Server.OdinServer;
+
+public class Offsite {
+    public static void main(String[] args){
+        OdinServer a = new OdinServer();
+        //4/10/18
+//        a.addEmployee();
+
+
+    }
+}
