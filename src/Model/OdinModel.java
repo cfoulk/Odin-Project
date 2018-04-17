bebaac9dd3ebc14493186d5c8ee9e1dfef6e00dd
Yusuf Amani <yusufamani@csus.edu>
2018-04-17 15:42:53 -0700

    fixed editEmployee method to check for user validation

--- src/Model/OdinModel.java
+++ src/Model/OdinModel.java
@@ -41,8 +41,11 @@ public class OdinModel implements OdinInterface
             emp = OS.getEmployee(employeeID);
             if(emp != null)
             {
-                OS.editEmployee(employeeID, name, position, groupID, username, password);
-                return true;
+                emp = OS.getEmployee(username);
+                if(emp ==null ) {
+                    OS.editEmployee(employeeID, name, position, groupID, username, password);
+                    return true;
+                }
             }
         }
         catch(Exception e) { e.printStackTrace(); }
@@ -52,10 +55,12 @@ public class OdinModel implements OdinInterface
     public boolean editProject(int projectID, String name, String dueDate, int groupID, int projectLeadID, String description, String status)
     {
         Project proj;
+        Employee lead;
         try
         {
+            lead = OS.getEmployee(projectLeadID);
             proj = OS.getProject(projectID);
-            if ( proj != null)
+            if ( proj != null && lead.position.compareTo("Project Lead") == 0)
             {
                 OS.editProject(projectID,name,dueDate,groupID,projectLeadID,description,status);
                 return true;
