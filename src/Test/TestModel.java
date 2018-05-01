db3d19880e38ea073724ae6b0d95a9110d4eab57
ramyasingam <ramyasrisingams@csus.edu>
2018-05-01 16:29:34 -0700

    Created writing function(to a file) to test server and model class.

--- src/Test/TestModel.java
+++ src/Test/TestModel.java
@@ -1,10 +1,38 @@
 package Test;
 import Model.OdinModel;
+import java.io.PrintWriter;
+import java.io.File;
+import java.io.*;
+import java.util.ArrayList;
+
 
 public class TestModel {
 
-    public static void main(String []args)
+    public static void main(String []args) throws Exception
     {
-        OdinModel model = new OdinModel;
+       try
+       {
+           PrintWriter printWriter = new PrintWriter("ModelServerTest");
+           printWriter.print("");
+           printWriter.close();
+
+           FileWriter fileWriter = new FileWriter("ModelServerTest");
+
+           BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+
+
+
+       }
+
+       catch (IOException e)
+       {
+           System.out.println(e);
+       }
+
+
+
+
     }
+
+
 }
