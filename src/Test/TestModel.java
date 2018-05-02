89ffdf234f0e2d1ffd58288978ec5d8761a6c7c8
ramyasingam <ramyasrisingams@csus.edu>
2018-05-01 22:30:24 -0700

    Removed filewriter code. Testing directly from serever instead.

--- src/Test/TestModel.java
+++ src/Test/TestModel.java
@@ -1,53 +1,26 @@
 package Test;
 import Model.OdinModel;
 import Server.Employee;
-import java.io.PrintWriter;
-import java.io.File;
-import java.io.*;
-import java.util.ArrayList;
-
+import javax.xml.transform.Result;
 
 public class TestModel {
 
-    public static void main(String []args) throws Exception
-    {
+    public static void main(String[] args) throws Exception {
+
         //Testing employee operations
         Employee employee;
-
         OdinModel model = new OdinModel();
-        model.addEmployee("John","Manager", 1234, "John", "doe");
-        employee =  model.getEmployee_Username(("John"));
+        model.addEmployee("John", "Manager", 1234, "John", "doe");
+        employee = model.getEmployee_Username(("John"));
 
-        if(model.editEmployee(employee.employeeID, employee.name, employee.position, employee.groupID, employee.username, "xyz")
+        if (model.editEmployee(employee.employeeID, employee.name, employee.position, employee.groupID, employee.username, "xyz"))
         {
-            System.out.println("model.addEmployee test passed")
+            System.out.println("model.addEmployee test passed");
+        }
+        else {
+            System.out.println("model.addEmployee test failed");
         }
 
-       try
-       {
-           PrintWriter printWriter = new PrintWriter("ModelServerTest");
-           printWriter.print("");
-           printWriter.close();
-
-           FileWriter fileWriter = new FileWriter("ModelServerTest");
-
-           BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
-
-           ArrayList<String> elements = new ArrayList<>();
-
-
-
-       }
-
-       catch (IOException e)
-       {
-           System.out.println(e);
-       }
-
-
-
-
     }
-
-
 }
+
