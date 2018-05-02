9188cfb3392477a1215b8730b196b5b0cdfcf3d8
ramyasingam <ramyasrisingams@csus.edu>
2018-05-01 21:47:01 -0700

    Testing addEmployee method in Model class.

--- src/Test/TestModel.java
+++ src/Test/TestModel.java
@@ -1,5 +1,6 @@
 package Test;
 import Model.OdinModel;
+import Server.Employee;
 import java.io.PrintWriter;
 import java.io.File;
 import java.io.*;
@@ -10,6 +11,18 @@ public class TestModel {
 
     public static void main(String []args) throws Exception
     {
+        //Testing employee operations
+        Employee employee;
+
+        OdinModel model = new OdinModel();
+        model.addEmployee("John","Manager", 1234, "John", "doe");
+        employee =  model.getEmployee_Username(("John"));
+
+        if(model.editEmployee(employee.employeeID, employee.name, employee.position, employee.groupID, employee.username, "xyz")
+        {
+            System.out.println("model.addEmployee test passed")
+        }
+
        try
        {
            PrintWriter printWriter = new PrintWriter("ModelServerTest");
@@ -19,6 +32,8 @@ public class TestModel {
            FileWriter fileWriter = new FileWriter("ModelServerTest");
 
            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+
+           ArrayList<String> elements = new ArrayList<>();
 
 
 
