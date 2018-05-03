d296e7971dd720241d54bba478faabe8d0c449f4
Yusuf Amani <yusufamani@csus.edu>
2018-05-03 12:54:54 -0700

    learning and testing the contoller for testUI

---
+++ src/App/gui/component/TestUIController.java
@@ -0,0 +1,31 @@
+package App.gui.component;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Menu;
+
+public class TestUIController {
+    @FXML
+    private Menu exit;
+
+    @FXML
+    private Button add;
+
+    @FXML
+    private Button edit;
+
+    @FXML
+    void edituser(ActionEvent event) {
+
+    }
+
+    @FXML
+    void exitProgram(ActionEvent event) {
+
+    }
+
+    @FXML
+    void makeuser(ActionEvent event) {
+
+    }
+}
