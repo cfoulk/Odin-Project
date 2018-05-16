02c016ba2c1527ed718868771dd6dfeedf4a4d64
ramyasingam <ramyasrisingams@csus.edu>
2018-05-16 08:30:46 -0700

    Fixed errors in addtask population to server.

--- src/Test/SeedOdinData.java
+++ src/Test/SeedOdinData.java
@@ -51,29 +51,29 @@ public class SeedOdinData {
 
         //--------------------------------------------------------------task data-----------------------------------------------------------------
         //Project 1 Tasks
-        model.addTask("Fix Time","2018-02-09",1, "1, 2", "Time taken", 2, "Active"); //1
-        model.addTask("Find payroll","2018-07-12",1, "3, 4", "payroll calculation", 3, "Inactive"); //2
-        model.addTask("Check payroll","2018-08-01",1, "2, 3, 4", "compare to previous payroll", 1, "Active"); //3
+        model.addTask("Fix Time","2018-02-09",1, ",1,2", "Time taken", 2, "Open"); //1
+        model.addTask("Find payroll","2018-07-12",1, ",3,4", "payroll calculation", 3, "Closed"); //2
+        model.addTask("Check payroll","2018-08-01",1, ",2,3,4", "compare to previous payroll", 1, "Open"); //3
 
         //Project 2 Tasks
-        model.addTask("Organize Files","2018-04-11",2, "3, 5", "File Organization", 4, "Active"); //4
-        model.addTask("Update Files","2018-07-12",2, "9, 10", "File update", 3, "Active"); //5
-        model.addTask("Check Files","2018-07-23",2, "2, 6, 4", "compare to previous payroll", 1, "Active"); //6
+        model.addTask("Organize Files","2018-04-11",2, ",3,5", "File Organization", 4, "Open"); //4
+        model.addTask("Update Files","2018-07-12",2, ",9,10", "File update", 3, "Open"); //5
+        model.addTask("Check Files","2018-07-23",2, ",2,6,4", "compare to previous payroll", 1, "Open"); //6
 
         //Project 3 Tasks
-        model.addTask("Computer fixture","2018-05-05",3, "5, 9", "File Organization", 3, "Inactive"); //7
-        model.addTask("Fix computer problems","2018-05-17",3, "8, 3", "File update", 5, "Active"); //8
-        model.addTask("Check computer problems.","2018-06-11",3, "1, 3, 10", "check if comp problems fixed", 1, "Active"); //9
+        model.addTask("Computer fixture","2018-05-05",3, ",5,9", "File Organization", 3, "Closed"); //7
+        model.addTask("Fix computer problems","2018-05-17",3, ",8,3", "File update", 5, "Open"); //8
+        model.addTask("Check computer problems.","2018-06-11",3, ",1,3,10", "check if comp problems fixed", 1, "Open"); //9
 
         //Project 4 Tasks
-        model.addTask("Manage Money","2018-08-16",4, "2, 9", "Manage money in company", 5, "Active"); //10
-        model.addTask("Find Financial Issues","2018-08-13",4, "1, 10", "find where money should be", 5, "Active"); //11
-        model.addTask("Resolve Financial Issues.","2018-09-14",4, "6, 5, 3", "fix the financial issues", 1, "Active"); //12
+        model.addTask("Manage Money","2018-08-16",4, ",2,9", "Manage money in company", 5, "Open"); //10
+        model.addTask("Find Financial Issues","2018-08-13",4, ",1,10", "find where money should be", 5, "Open"); //11
+        model.addTask("Resolve Financial Issues.","2018-09-14",4, ",6,5,3", "fix the financial issues", 1, "Open"); //12
 
         //Project 5 Tasks
-        model.addTask("Identify library contents","2018-05-10",5, "7, 5", "Find contents of library", 3, "Active"); //13
-        model.addTask("Organize Library","2018-05-11",5, "1, 4", "organize existing library", 3, "Active"); //14
-        model.addTask("Get library checked","2018-05-19",5, "2, 5, 9", "check if library is set", 1, "Active"); //15
+        model.addTask("Identify library contents","2018-05-10",5, ",7,5", "Find contents of library", 3, "Open"); //13
+        model.addTask("Organize Library","2018-05-11",5, ",1,4", "organize existing library", 3, "Open"); //14
+        model.addTask("Get library checked","2018-05-19",5, ",2,5,9", "check if library is set", 1, "Open"); //15
 
         //--------------------------------------------------------------worklog data-----------------------------------------------------------------
 
