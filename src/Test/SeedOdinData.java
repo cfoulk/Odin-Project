d2b1e9fe8be3c63aa58e324bbe62dc7369947755
ramyasingam <ramyasrisingams@csus.edu>
2018-05-16 00:40:39 -0700

    Added task data to populate to server.

--- src/Test/SeedOdinData.java
+++ src/Test/SeedOdinData.java
@@ -27,22 +27,50 @@ public class SeedOdinData {
 
         //user: Project Lead
         model.addEmployee("Jane", "Project Lead", 1, "JaneUser", "Janepwd", "Active"); //11
-        model.addEmployee("Zoe", "Project Lead", 2, "ZoeUser", "Zoepwd", "Active"); /12
+        model.addEmployee("Zoe", "Project Lead", 2, "ZoeUser", "Zoepwd", "Active"); //12
         model.addEmployee("Simon", "Project Lead", 3, "SimonUser", "Simonpwd", "Active"); //13
         model.addEmployee("George", "Project Lead", 4, "GeorgeUser", "Georgepwd", "Active"); //14
-        model.addEmployee("Max", "Project Lead", 5, "MaxUser", "Maxpwd", "Active"); /15
+        model.addEmployee("Max", "Project Lead", 5, "MaxUser", "Maxpwd", "Active"); //15
 
         //user: Manager
         model.addEmployee("Sam", "Manager", 1, "SamUser", "Sampwd", "Active"); //16
         model.addEmployee("Alex", "Manager", 2, "AlexUser", "Alexpwd", "Active"); //17
         model.addEmployee("Jim", "Manager", 3, "JimUser", "Jimpwd", "Active"); //18
         model.addEmployee("Tom", "Manager", 4, "TomUser", "Tompwd", "Active"); //19
-        model.addEmployee("Maddy", "Manager", 5, "MaddyUser", "Maddypwd", "Active");*/ //20
+        model.addEmployee("Maddy", "Manager", 5, "MaddyUser", "Maddypwd", "Active"); */ //20
 
 
 
-        //--------------------------------------------------------------employee data--------------------------------------------------------------
-        model.addProject("Time Management", "2018-02-05", "1", "11")
+        //--------------------------------------------------------------project data--------------------------------------------------------------
+        /*model.addProject("Time Management", "2018-02-05",1, 11, "Manage Time", "Open");
+        model.addProject("Payroll Management", "2018-03-17",2, 12, "Payroll fix", "Closed");
+        model.addProject("Student Counseling", "2018-01-22",3, 13, "counsel", "Open");
+        model.addProject("Movie Management", "2018-07-13",4, 14, "manage movies", "Open");
+        model.addProject("Library Management", "2018-12-31",5, 15, "library file fix", "Closed"); */
+
+
+        //--------------------------------------------------------------task data-----------------------------------------------------------------
+        //Project 1
+        model.addTask("Fix Time","2018-02-09",1, "1, 2", "Time taken", 2, "Active");
+        model.addTask("Find payroll","2018-07-12",1, "3, 4", "payroll calculation", 3, "Inactive");
+        model.addTask("Check payroll","2018-08-01",1, "2, 3, 4", "compare to previous payroll", 1, "Active");
+
+        //Project 2
+        model.addTask("Organize Files","2018-04-11",2, "3, 5", "File Organization", 4, "Active");
+        model.addTask("Update Files","2018-07-12",2, "1, 4", "File update", 3, "Active");
+        model.addTask("Check Files","2018-07-23",2, "2, 5, 4", "compare to previous payroll", 1, "Active");
+
+        //Project 3
+        model.addTask("Computer fixture","2018-05-05",3, "1, 4", "File Organization", 3, "Inactive");
+        model.addTask("Fix computer problems","2018-05-17",3, "2, 3", "File update", 5, "Active");
+        model.addTask("Check computer problems.","2018-06-11",3, "1, 3, 4", "check if comp problems fixed", 1, "Active");
+
+
+
+
+
+
+
 
 
     }
