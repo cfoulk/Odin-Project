ebaf6995b07f42ad396887de1431950f60650d9d
ramyasingam <ramyasrisingams@csus.edu>
2018-05-15 22:06:39 -0700

    Added users: employees only.

--- src/Test/SeedOdinData.java
+++ src/Test/SeedOdinData.java
@@ -13,10 +13,22 @@ public class SeedOdinData {
 
         OdinModel model = new OdinModel();
         //see employee data
-        model.addEmployee("John", "Manager", 1234, "JohnUser", "doe", "Active");
-        model.addEmployee("Sarah", "Manager", 1234, "SarahJones", "Sarahpwd", "Active");
-        model.addEmployee("Tim", "Project Lead", 1234, "TimSmith", "Timpwd", "Active");
-        model.addEmployee("Test4", "Manager", 1234, "JohnUser", "doe", "Active");
+        model.addEmployee("John", "Employee", 1234, "JohnUser", "doe", "Active");
+        model.addEmployee("Sarah", "Employee", 1234, "SarahJones", "Sarahpwd", "Active");
+        model.addEmployee("Tim", "Employee", 1234, "TimSmith", "Timpwd", "Active");
+        model.addEmployee("Bob", "Employee", 1234, "BobUser", "Bobpwd", "Active");
+        model.addEmployee("Glen", "Employee", 1234, "GlenUser", "Glenpwd", "Active");
+        model.addEmployee("Joe", "Employee", 1234, "JoeUser", "Joepwd", "Active");
+        model.addEmployee("Chris", "Employee", 1234, "ChrisUser", "Chrispwd", "Active");
+        model.addEmployee("Julia", "Employee", 1234, "JuliaUser", "Juliapwd", "Active");
+        model.addEmployee("Hannah", "Employee", 1234, "HannahUser", "Hannahpwd", "Active");
+        model.addEmployee("Lauren", "Employee", 1234, "LaurenUser", "Laurenpwd", "Active");
+
+
+
+
+
+
 
     }
 
